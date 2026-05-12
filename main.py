919f68c8a53afd810fc7ad1188a3571f522f56cf
Jay Joshi <jaykumarjoshi37@gmail.com>
2026-05-12 14:39:33 +0530

    fixed railway mysql config

--- main.py
+++ main.py
@@ -14,9 +14,15 @@ app.secret_key = os.getenv("SECRET_KEY")
 
 # MySQL Config
 app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+
+app.config['MYSQL_PORT'] = int(
+    os.getenv("MYSQL_PORT")
+)
+
 app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+
 app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
-app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT"))
+
 app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 
 mysql = MySQL(app)
