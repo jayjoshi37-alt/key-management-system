d59a6836192734602ef11bb9ae258667bba9bf93
Jay Joshi <jaykumarjoshi37@gmail.com>
2026-05-12 14:35:59 +0530

    fixed render mysql env

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, redirect, session
 from flask_mysqldb import MySQL
-from dotenv import load_dotenv
-import os
 from utils.key_generator import generate_api_key
 from flask import jsonify
 import requests
+from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
@@ -16,6 +16,7 @@ app.secret_key = os.getenv("SECRET_KEY")
 app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
 app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
 app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+app.config['MYSQL_PORT'] = int(os.getenv("MYSQL_PORT"))
 app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
 
 mysql = MySQL(app)
